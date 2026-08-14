0a399f537c9530fdcee11fd72e6492495487684e
Calteo <matthias@famger.de>
2026-08-14 19:34:41 +0200

    typo in init script

--- scripts/initialize.ps1
+++ scripts/initialize.ps1
@@ -1,5 +1,5 @@
 echo "Script called"
 ls env:
 
-echo "Create solution .\src\${env:PROJECT_NAME].slnx"
-Rename-Item .\src\Solution.slnx ".\src\${env:PROJECT_NAME].slnx"
+echo "Create solution .\src\${env:PROJECT_NAME}.slnx"
+Rename-Item .\src\Solution.slnx ".\src\${env:PROJECT_NAME}.slnx"
