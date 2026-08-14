3b9097dbbaf7e7b3bd5e09ab1423ef053011dd47
Calteo <matthias@famger.de>
2026-08-14 22:03:28 +0200

    init

--- scripts/initialize.ps1
+++ scripts/initialize.ps1
@@ -27,12 +27,12 @@ Write-Output "::group::`e[33mScript called with environment`e[0m"
 ls env:
 Write-Output "::endgroup::"
 
-Write-Output "::group::`e[34mCreate solution ${env:PROJECT_NAME}.slnx`e[0m"
+Write-Output "`e[34mCreate solution ${env:PROJECT_NAME}.slnx`e[0m"
 Rename-Item src\Solution.slnx "${env:PROJECT_NAME}.slnx"
-Write-Output "::endgroup::"
 
 Write-Output "::group::`e[34mPatch README.md`e[0m"
 Move-Item README-Project.md README.md -Force
+Replace README.md
 Write-Output "::endgroup::"
 
 Write-Output "::group::`e[34mPatch documentation`e[0m"
@@ -40,9 +40,8 @@ Replace docfx_project\index.md
 Replace docfx_project\docfx.json
 Write-Output "::endgroup::"
 
-Write-Output "::group::`e[34mRemove initialization`e[0m"
+Write-Output "`e[34mRemove initialization`e[0m"
 Remove-Item .github\workflows\initialization.yml -Force
 Remove-Item $PSCommandPath -Force
-Write-Output "::endgroup::"
 
 Write-Host "`e[32mInitialization complete`e[0m"
